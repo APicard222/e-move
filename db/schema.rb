b9436d2c8a24869901672a68e03772b9a1c101fe
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-22 16:39:46 +0100

    migration remove password

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_152619) do
+ActiveRecord::Schema.define(version: 2021_11_22_153102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
-    t.string "password"
     t.string "address"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false

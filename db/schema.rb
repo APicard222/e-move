bfbadbe61482989309b94e94e220bb2604b3d5c6
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-22 14:18:41 +0100

    add vehicle model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_130247) do
+ActiveRecord::Schema.define(version: 2021_11_22_131719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,17 @@ ActiveRecord::Schema.define(version: 2021_11_22_130247) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "vehicles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "preice"
+    t.decimal "avg_rating"
+    t.boolean "available"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
+  end
+
+  add_foreign_key "vehicles", "users"
 end

8def0a7c54ab612834d708ea6733e968ae3bbc70
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-22 15:29:09 +0100

    model user and vehicle validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :user_name, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true
+  validates :address, presence: true
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,7 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end

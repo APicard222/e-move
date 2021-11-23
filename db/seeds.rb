06cdded0377214e7c260dac4285962c86e30a66c
Alexandre Picard <alex.picard@bluewin.ch>
2021-11-23 11:28:17 +0100

    Seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 puts "Starting"
-User.create(user_name: "Jacqouilles", address: "Lausanne", email: "jaques.chirac@gmail.com", encrypted_password: "123456")
-User.create(user_name: "Henry", address: "Le Havre", email: "henry.ford@gmail.com", encrypted_password: "123456")
-User.create(user_name: "Boris", address: "Boulogne", email: "carls.chic@gmail.com", encrypted_password: "123456")
+User.create!(user_name: "Jacqouilles", address: "Lausanne", email: "jaques.chirac@gmail.com", password: "123456")
+User.create!(user_name: "Henry", address: "Le Havre", email: "henry.ford@gmail.com", password: "123456")
+User.create!(user_name: "Boris", address: "Boulogne", email: "carls.chic@gmail.com", password: "123456")
 
-Vehicle.create(title: "Boosted Board", description: "400W plein axe", price: 152, user_id: 2)
-Vehicle.create(title: "Speedy Bike", description: "La classe holandaise", price: 1532, user_id: 1)
-Vehicle.create(title: "Troteenette", description: "Pur plaisir", price: 15, user_id: 1)
+Vehicle.create!(title: "Boosted Board", description: "400W plein axe", price: 152, user_id: 2)
+Vehicle.create!(title: "Speedy Bike", description: "La classe holandaise", price: 1532, user_id: 1)
+Vehicle.create!(title: "Troteenette", description: "Pur plaisir", price: 15, user_id: 1)
 puts "Ended"

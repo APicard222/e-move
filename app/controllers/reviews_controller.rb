18821edfb39e8083911a2294ebbafaa988a29218
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-25 13:20:25 +0100

    added view, and it creates a review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  def new
+    @vehicle = Vehicle.find(params[:vehicle_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.vehicle = Vehicle.find(params[:vehicle_id])
+    @review.user = current_user
+
+    if @review.save
+      redirect_to reservations_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+end

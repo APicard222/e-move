870728a56566e2c42a094ede5365d715349c7787
Alexandre Picard <alex.picard@bluewin.ch>
2021-11-25 15:33:21 +0100

    Dashboard created

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
     resources :reservations, only: %i[new create]
   end
   resources :reservations, only: %i[index destroy]
+  resources :users do
+    collection do
+      get :dashboard
+    end
+  end
 end
